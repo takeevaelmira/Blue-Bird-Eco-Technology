0147fea81bbde7c0574a721a0e9427856b29f98b
ibrahim__007 <khanzhynmuslim@gmail.com>
2026-09-08 18:29:15 +0300

    Refactor server.js for improved car search functionality

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ const server = http.createServer((req, res) => {
             const requestPayload = {
                 providerId: clientData.providerId || 2,
                 offset: clientData.offset || 0,
-                limit: 100,
+                limit: 1000,
                 filters: {
                     saleOnly: true,
                     yearMin: requestedYearMin,
